fbfef08276c6d3a70edfc9fb1fb54bd6f804b114
Kiran Belekar <kiran.belekar@acellere.com>
2020-06-25 12:28:06 +0530

    Add files via upload
    Has Below Code issues:
    1. Invalid Logging Class Name: (KPI: Usage, Severity: High)
    2. Cross-site Request Forgery Token Validation: (KPI: Security, Severity: Info)
    3. Class Implements ICloneable: (KPI: Reliability, Severity: High)
    4. Vulnerable Encryption Mode: (KPI: Security, Severity: critical)
    5. Weak Encryption: (KPI: Security, Severity: critical)

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChecksSecurityRelatedTestBed
+{
+    class CodeCheckTestCases
+    {
+        private static ILog log = LogManager.GetLogger("HomeController");  //InvalidLoggingClassNameRule voilation here
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var tripleDES = new TripleDESCryptoServiceProvider()) //Weak encryption here
+                {
+                    //...
+                }
+            }
+            catch (Exception e)
+            {
+               
+            }
+        }
+
+        [HttpPost]
+        public IActionResult ChangePassword()           //CSRF voilation here
+        {
+            // ...
+            return View();
+        }
+    }
+
+    //ClassImplementsICloneable voilation
+    public class Class1 : ICloneable
+    {
+        public int a;
+        public int b;
+
+        public Class1(int aa, int bb)
+        {
+            AesManaged aes = new AesManaged
+            {
+                KeySize = 128,
+                BlockSize = 128,
+                Mode = CipherMode.ECB,  // Noncompliant			
+                Padding = PaddingMode.PKCS7
+            };
+        }
+
+        public string ToString()
+        {
+            return "(" + a + "," + b + ")";
+        }
+
+        public virtual object Cloning()
+        {
+            return new Class1(a, b);
+        }
+        object ICloneable.Clone()
+        {
+            return Cloning();
+        }
+    }
+}
